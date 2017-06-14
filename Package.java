26358100208724405acd2c0e02fa8532f1ef1e6f
Thomas Gwozdz <thomaspgwozdz@gmail.com>
2017-06-14 08:44:15 +0300

    removed family.java

---
+++ Package.java
@@ -0,0 +1,25 @@
+/******************************************************************************/
+/* This file was created by Thomas Gwozdz for use at the International        */
+/* University of East Africa in Kampala, Uganda.  It may be used as study     */
+/* material for private individuals or with permission in an educational      */
+/* setting.  Contact the creator at tg0640@gmail.com                          */
+/******************************************************************************/
+/* Name:                                                                      */
+/* OOP1 Section:                                                              */
+/******************************************************************************/
+/* This is a diagnostic test designed to see how well you have learned the    */
+/* concepts in the first weeks of OOP1.  THIS IS NOT FOR A GRADE, but it MUST */
+/* be completed.  Try to complete each task to the best of your ability, but  */
+/* if you cannot figure something out, please move on quickly.                */
+/******************************************************************************/
+
+
+
+public class Package {
+    public static void main(String[] args) {
+        // In Java, 
+        System.out.println("Welcome to your second program!");
+
+        }
+
+    }
